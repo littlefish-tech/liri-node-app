01ea53b4ebc322956275e72f9f9311f37520d681
Yimeng Yu <yimeng.yu@gmail.com>
2019-11-28 10:15:09 -0800

    update files

--- keys.js
+++ keys.js
@@ -4,3 +4,10 @@ exports.spotify = {
   id: process.env.SPOTIFY_ID,
   secret: process.env.SPOTIFY_SECRET
 };
+
+var axios = require("axios");
+axios.get("http://www.omdbapi.com/?i=tt3896198&apikey=429247b6").then(
+    function(response){
+        console.log()
+    }
+)
--- liri.js
+++ liri.js
@@ -1,3 +1,8 @@
-require("dotenv").config();
+var doteNv = require("dotenv").config();
 var keys = require("./keys.js");
-var spotify = new Spotify(keys.spotify);
+var spotify = new Spotify(
+    //keys.spotify)
+    concert-this,
+    spotify-this-song,
+    movie-this,
+    do-what-it-says);
